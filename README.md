c9112d52a357ed4c3c7d4b596738aa9aab90703c
Wang <iuncis2015@gmail.com>
2022-10-09 14:48:53 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # jquarto-template
+
+### From GitHub Repo to RStudio Project
+
+-   RStudio Project
+
+```{r eval=F}
+usethis::create_from_github(
+  "https://github.com/remjw/jquarto-template.git",
+  destdir = "~/Documents/quarto/",
+  rstudio=TRUE
+)
+```
+
+-   renv
+
+```{r eval=F}
+renv::init() # from renv.lock file
+renv::snapshot()
+```
+
+-   .venv
+
+```{bash eval=F}
+# create
+python3 -m venv .venv
+# activate
+source .venv/bin/activate
+# install
+pip install -r requirements.txt
+# snapshot
+python3 -m pip freeze > requirements.txt
+```
+
+-   variables.yml
+
+- Preview
+
+```{bash}
+#| eval: False
+(.venv) (quarto) $ quarto preview jquarto-template
+```
+
--- chunk_setup_db_module.py
+++ chunk_setup_db_module.py
@@ -1,11 +1,11 @@
 ## ---- python-module-db ---
-mysql_host = "127.0.0.1"
-mysql_user = "root"
-mysql_pwd = "apple"
-mysql_port = 3306
+#mysql_host = "127.0.0.1"
+#mysql_user = "root"
+#mysql_pwd = "apple"
+#mysql_port = 3306
 
+import yaml
 import graphviz
-
 import os
 import pandas as pd
 #pip install mysql-connector-python
@@ -15,6 +15,21 @@ from mysql.connector import errorcode
 from sqlalchemy import create_engine, types
 import pymysql
 
+def load_conf_file(config_file):
+  with open(config_file, "r") as f:
+    config = yaml.safe_load(f)
+    print(config)
+    info_conf = config["info"]
+    mysql_conf = config["mysql"]
+  return info_conf, mysql_conf
+ 
+info_conf, mysql_conf = load_conf_file("_variables.yml")
+mysql_host = mysql_conf["host"]
+mysql_port = mysql_conf["port"]
+mysql_user = mysql_conf["user"]
+mysql_pwd = mysql_conf["password"]
+
+
 def connect_mysql(db_name):
     try:
         db = mc.connect(
@@ -22,7 +37,7 @@ def connect_mysql(db_name):
             port = mysql_port,
             user = mysql_user,
             password = mysql_pwd,
-            database=db_name
+            database = db_name
         )
     except mc.Error as e:
         print(f"something went wrong:{e}")    
